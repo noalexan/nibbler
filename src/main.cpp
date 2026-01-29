52d35b82624bd8553a9fe09ad661c2725bccf87b
Noah Alexandre <noah.alxdr+github@gmail.com>
2026-01-29 16:35:17 +0100

    feat(args): Parse `width` and `height` from `argv`

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
+#include <unistd.h>
 
-int main(void) {
-  std::cout << "Hello, World!" << std::endl;
-  return 0;
+#define DEFAULT_HEIGHT 64
+#define DEFAULT_WIDTH  64
+
+int width  = DEFAULT_WIDTH;
+int height = DEFAULT_HEIGHT;
+
+int main(int argc, char *argv[])
+{
+	int opt;
+	while ((opt = getopt(argc, argv, "w:h:")) != -1) {
+		switch (opt) {
+		case 'w':
+			width = std::stoi(optarg);
+			break;
+		case 'h':
+			height = std::stoi(optarg);
+			break;
+		default: /* '?' */
+			std::cerr << std::endl
+			          << "Usage: " << argv[0] << " -w <width> -h <height>" << std::endl;
+			exit(EXIT_FAILURE);
+		}
+	}
+
+	std::cout << "Width: " << width << ", height: " << height << std::endl;
+
+	return 0;
 }
